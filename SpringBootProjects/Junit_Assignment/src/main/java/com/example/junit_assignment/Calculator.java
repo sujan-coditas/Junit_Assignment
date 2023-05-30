3acc5c29c587600326f5938fe54200bad44bc856
Sujan Shaikh <sujan.shaikh@coditas.com>
2023-05-30 11:38:23 +0530

    junit assignment

---
+++ SpringBootProjects/Junit_Assignment/src/main/java/com/example/junit_assignment/Calculator.java
@@ -0,0 +1,18 @@
+package com.example.junit_assignment;
+
+public class Calculator {
+    public int sum(int number1,int number2){
+        return number1+number2;
+    }
+    public int sub(int number1,int number2){
+        return number1-number2;
+    }
+    public int multiply(int number1,int number2){
+        return number1*number2;
+    }
+    public int divide(int number1,int number2){
+        return number1/number2;
+    }
+
+
+}
---
+++ SpringBootProjects/Junit_Assignment/src/test/java/com/example/junit_assignment/CalculatorTest.java
@@ -0,0 +1,33 @@
+package com.example.junit_assignment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(2, 3);
+        assertEquals(5, result);
+    }
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sub(10, 3);
+        assertEquals(7, result);
+    }  @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(2, 3);
+        assertEquals(6, result);
+    }
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        int result = calculator.divide(10, 3);
+        assertEquals(3, result);
+    }
+
+
+}
